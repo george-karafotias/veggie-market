e20aabb404cce89969ba46aadf7c8840984ff033
George Karafotias <40838327+george-karafotias@users.noreply.github.com>
2025-06-06 23:23:10 +0300

    Implementing a markets controller to return the markets info from the API.

---
+++ core/VeggieMarketApi/Controllers/MarketsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using VeggieMarketDataStore.DbServices;
+using VeggieMarketDataStore.Models;
+
+namespace VeggieMarketApi.Controllers
+{
+    public class MarketsController : ApiController
+    {
+        private readonly MarketDbService marketDbService;
+        private const string ROUTE_URL = "markets/";
+
+        public MarketsController()
+        {
+            marketDbService = DbServiceProvider.GetDataStorageService().MarketDbService;
+        }
+
+        [HttpGet]
+        [Route(ROUTE_URL)]
+        public IEnumerable<Market> GetAllMarkets()
+        {
+            return marketDbService.GetMarkets();
+        }
+
+        [HttpGet]
+        [Route(ROUTE_URL + "{marketName}")]
+        public IHttpActionResult GetMarket(string marketName)
+        {
+            Market market = marketDbService.GetMarket(marketName);
+            if (market == null)
+            {
+                return NotFound();
+            }
+            return Ok(market);
+        }
+    }
+}
